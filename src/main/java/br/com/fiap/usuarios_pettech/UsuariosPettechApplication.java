040d9b56f5f312e169ccd0753f87de7b6e7f67a4
David Nazare <dsnazare@icloud.com>
2024-11-09 20:19:26 -0300

    Versao inicial

---
+++ src/main/java/br/com/fiap/usuarios_pettech/UsuariosPettechApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.usuarios_pettech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosPettechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosPettechApplication.class, args);
+	}
+
+}
